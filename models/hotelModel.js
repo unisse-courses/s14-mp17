f1c3e008ad7386c0851b3c8e9e1636a7cd078f33
Lexie Mangune <lexiemangune@Lexies-MacBook-Air.local>
2020-05-02 11:01:59 +0800

    updated for mp3

--- models/hotelModel.js
+++ models/hotelModel.js
@@ -18,27 +18,21 @@ module.exports = hotelModel;
 // get all hotels from the collection
 exports.getAll = function(sort, next) {
     hotelModel.find({}).sort(sort).exec(function(err, result) {
-        if(err) throw err;
-
         var hotelObjects = [];
         result.forEach(function(doc) {
             hotelObjects.push(doc.toObject());
         });
-
         next(hotelObjects);
     });
 };
 
 // search a hotel
-exports.searchHotel = function(query, next) {
+exports.search = function(query, next) {
     hotelModel.find(query, function(err, hotels) {
-        if(err) throw err;
-
         var hotelObjects = [];
         result.forEach(function(doc) {
             hotelObjects.push(doc.toObject());
         });
-
         next(hotelObjects);
     });
 };
