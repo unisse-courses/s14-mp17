c3721ed0af77b0341ddb52bef39e088fd4d3bbfa
Lexie Mangune <lexiemangune@Lexies-MacBook-Air.local>
2020-05-02 20:26:21 +0800

    updated for mp3

--- models/bookingModel.js
+++ models/bookingModel.js
@@ -1,7 +1,7 @@
 const mongoose = require('./connection');
 
 const bookingSchema = new mongoose.Schema({
-    id: { type: Int32Array },
+    id: { type: Int32Array, required: true },
     name: { type: mongoose.Schema.Types.ObjectId, ref: 'hotelModel', required: true },
     username: { type: mongoose.Schema.Types.ObjectId, ref: 'userModel', required: true },
     checkIn: { type: Date, required: true },    
