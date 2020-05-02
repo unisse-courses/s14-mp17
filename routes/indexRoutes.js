1c911556897f098830862350b31c4c117e7cbb84
Lexie Mangune <lexiemangune@Lexies-MacBook-Air.local>
2020-05-02 21:22:37 +0800

    updated for mp3

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -4,15 +4,14 @@ const { isPublic } = require('../middlewares/checkAuth.js');
 
 // GET home route
 router.get('/', isPublic, (req, res) => {
-    res.render('home', {title: 'Find your ideal hotel at an affordable price!'});
+    hotelController.getAllHotels((posts) => {
+        res.render('home', {title: 'Find your ideal hotel at an affordable price!'});
+    });
 });
 
 // GET about route
 router.get('/about', isPublic, (req, res) => {
     res.render('about', {title: 'About'});
 });
-
-// POST home route
-router.post('/', isPublic, hotelController.getAllHotels);
 
 module.exports = router;
