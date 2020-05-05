552cb9638c40a28503e50600b7b69c911343ee99
Lexie Mangune <lexiemangune@Lexies-MacBook-Air.local>
2020-05-05 21:18:07 +0800

    updated for mp3

--- routes/searchRoutes.js
+++ routes/searchRoutes.js
@@ -3,9 +3,9 @@ const bookingController = require('../controllers/bookingController');
 const { isPublic } = require('../middlewares/checkAuth.js');
 
 // GET routes
-//router.get('/searchresults', isPublic, (req, res) => {
-//    res.render('searchresults', { title: 'Search Results' });
-//});
+router.get('/searchresults', isPublic, (req, res) => {
+    res.render('searchresults', { title: 'Search Results' });
+});
 
 // POST routes
 router.post('/searchresults', isPublic, bookingController.getAllAvailable);
