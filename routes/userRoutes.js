065d67c5dc7b03c7f93da6d9a74128e70aaa7984
Lexie Mangune <lexiemangune@Lexies-MacBook-Air.local>
2020-05-05 17:53:54 +0800

    updated for mp3

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,13 +1,13 @@
 const router = require('express').Router();
 const bookingController = require('../controllers/bookingController');
-//const userController = require('../controllers/userController');
+const bookingModel = require('../models/bookingModel');
 const { isPrivate } = require('../middlewares/checkAuth.js');
 
 // GET user manage bookings route
 router.get('/usermanagebooking', isPrivate, (req, res) => {
     const name = req.session.name;
 
-    bookingController.getUserBookings(name, (bookings) => {
+    bookingModel.getByUser(name, (bookings) => {
         res.render('usermanagebooking', { title: 'Dashboard: Manage Your Bookings', bookings: bookings });
     });
 });
@@ -27,6 +27,6 @@ router.get('/usercreatebooking', isPrivate, (req, res) => {
 });
 
 // POST user create bookings route
-router.post('/usercreatebooking', isPrivate, bookingController.getAllBookings);
+//router.post('/usercreatebooking', isPrivate, bookingController.getAllBookings);
 
 module.exports = router;
