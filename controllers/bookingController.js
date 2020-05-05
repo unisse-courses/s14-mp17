0912da9e5cfef0a97717cb1fd64733a837df5ec0
Lexie Mangune <lexiemangune@Lexies-MacBook-Air.local>
2020-05-05 22:00:06 +0800

    updated for mp3

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -18,11 +18,10 @@ exports.getAllAvailable = (req, res) => {
     const hotelname = req.body.hotelname;
     const capacity = req.body.capacity;
 
-    console.log('marker1');
     bookingModel.getAvailable({hotelname, capacity, status: 'Available'}, (err, bookings) => {
         if (err) throw err;
-        console.log('marker2');
         res.render('searchresults', { bookings });
+        res.render('usercreatebooking', { bookings });
     })
 }
 
