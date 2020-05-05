68ace67879fef02da3e56bf00b7e306a3eb11526
Lexie Mangune <lexiemangune@Lexies-MacBook-Air.local>
2020-05-06 02:42:26 +0800

    updated for mp3

--- public/js/script.js
+++ public/js/script.js
@@ -16,14 +16,44 @@ $(document).ready(function() {
         parentDiv.append(userInfo);
     }
 
-    $.post('/adminmanageuser', function(data) {
-        var userListContainer = $('#user-table-body');
-        userListContainer.empty();
+    function addAdminBookingsDiv(item, parentDiv)
+    {
+        var bookingInfo = document.createElement('tr');
 
-        data.forEach((item, i), function() {
-            addUsersDiv(item, userListContainer);
+        var hotelnameHeading = document.createElement('td');
+        var usernameHeading = document.createElement('td');
+        var checkinHeading = document.createElement('td');
+        var checkoutHeading = document.createElement('td');
+        var capacityHeading = document.createElement('td');
+        var priceHeading = document.createElement('td');
+
+        $(hotelnameHeading).addClass('admin-booking-user-table');
+        $(usernameHeading).addClass('admin-booking-user-table');
+        $(checkinHeading).addClass('admin-booking-user-table');
+        $(checkoutHeading).addClass('admin-booking-user-table');
+        $(capacityHeading).addClass('admin-booking-user-table');
+        $(priceHeading).addClass('admin-booking-user-table');
+
+        bookingInfo.append(hotelnameHeading);
+        bookingInfo.append(usernameHeading);
+        bookingInfo.append(checkinHeading);
+        bookingInfo.append(checkoutHeading);
+        bookingInfo.append(capacityHeading);
+        bookingInfo.append(priceHeading);
+
+        parentDiv.append(bookingInfo);
+    }
+
+    $('#adminShowAllUsers').click(function() {
+        $.post('/adminmanageuser', function(data) {
+            var userListContainer = $('#user-table-body');
+            userListContainer.empty();
+    
+            data.forEach((item, i), function() {
+                addUsersDiv(item, userListContainer);
+            });
         });
-    })
+    });
 
     $('#adminSearchUser').click(function() {
         var name = $('#adminSearchUsername').val();
@@ -34,6 +64,30 @@ $(document).ready(function() {
 
             data.forEach((item, i), function() {
                 addUsersDiv(item, userListContainer);
+            });
+        })
+    });
+
+    $('#adminShowAllBookings').click(function() {
+        $.post('/adminmanagebooking', function(data) {
+            var bookingListContainer = $('#admin-booking-table-body');
+            bookingListContainer.empty();
+    
+            data.forEach((item, i), function() {
+                addAdminBookingsDiv(item, bookingListContainer);
+            });
+        });
+    });
+
+    $('#adminSearchBooking').click(function() {
+        var name = $('#adminSearchID').val();
+
+        $.post('/adminmanagebooking/search', {name: name}, function(data) {
+            var bookingListContainer = $('#admin-booking-table-body');
+            bookingListContainer.empty();
+
+            data.forEach((item, i), function() {
+                addAdminBookingsDiv(item, bookingListContainer);
             });
         })
     });
