ae421ec880eda589195f3ba5cc862dfd23558a28
Lexie Mangune <lexiemangune@Lexies-MacBook-Air.local>
2020-05-05 15:24:03 +0800

    updated for mp3

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -17,7 +17,7 @@ router.get('/usersearchbooking', isPrivate, (req, res) => {
 });
 
 router.get('/usercreatebooking', isPrivate, (req, res) => {
-    const name = req.body.hotelSearch;
+    const name = req.session.hotelSearch;
 
     res.render('usercreatebooking', {title: 'Search Results For', name});
 });
