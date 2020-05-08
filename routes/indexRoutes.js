df2afa69c1b197d7fbd3067d7643c820cfc32c1f
Lexie Mangune <lexiemangune@Lexies-MacBook-Air.local>
2020-05-08 17:29:59 +0800

    updated

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -3,7 +3,9 @@ const hotelController = require('../controllers/hotelController');
 const { isPublic, isPrivate } = require('../middlewares/checkAuth.js');
 
 // GET home route
-router.get('/', isPublic, hotelController.getAllHotels);
+router.get('/', isPublic, (req, res) => {
+    res.render('usermanagebooking', { title: 'Dashboard: Manage Your Bookings'});
+});
 
 // GET about route
 router.get('/about', isPublic, (req, res) => {
