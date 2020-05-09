6d026bed7d67cab831e8624cdcd544ae1be0fa10
Lexie Mangune <lexiemangune@Lexies-MacBook-Air.local>
2020-05-09 19:20:14 +0800

    mp3

--- controllers/hotelController.js
+++ controllers/hotelController.js
@@ -3,6 +3,6 @@ const hotelModel = require('../models/hotelModel');
 exports.getAllHotels = function(req, res) {
     const hotelname = req.body.hotelSearch;
     hotelModel.getAll(hotelname, function(err, hotels) {
-        res.render('home', { title: 'Find your ideal hotel at an affordable price!', hotels: hotels });
+        res.render('home', { title: 'Find your ideal hotel at an affordable price!', hotels: hotels, active: {logout: false} });
     });
 };
